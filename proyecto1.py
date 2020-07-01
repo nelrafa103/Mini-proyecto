839a5c9bd3569367b187f81bfbf26b68ea51a00b
Rafael Diaz <nelcidodiaz13@gmail.com>
2020-07-01 17:15:18 -0400

    version 1

--- proyecto1.py
+++ proyecto1.py
@@ -1,4 +1,9 @@
-def triangulo_de_fibonnaci():
-print ('hola')
 
-print triangulo_de_fibonnaci(23)
+def triangulo_de_fibonnaci(number): 
+ result =  number
+ if number <= 0:
+  return result
+ elif number > 1:
+  result = triangulo_de_fibonnaci(number - 1) + triangulo_de_fibonnaci(number - 2)  
+ return result
+print (triangulo_de_fibonnaci(1))
