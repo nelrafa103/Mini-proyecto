dd7d414d033eccf80d1ad0594d6281880493d971
Rafael Diaz <nelcidodiaz13@gmail.com>
2020-07-02 15:52:55 -0400

    Version 2.2

--- proyecto1.py
+++ proyecto1.py
@@ -1,11 +1,14 @@
 
+x = int(input())
 def fibonnaci_triangle(number): 
  print (number,end = ' ')
  if number <= 0 or number == 1:
   return number
  elif number > 1:
   return fibonnaci_triangle(number - 1) + fibonnaci_triangle(number - 2)
-print (fibonnaci_triangle(6))
+print (fibonnaci_triangle(x))
 
-
-def pascal_triangle(number)
+y = int(input())
+def pascal_triangle(number):
+ 
+print (pascal_triangle(y))
