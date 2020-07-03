4245d2a15b3c25116ee3830791ad1966410711e3
Rafael Diaz <nelcidodiaz13@gmail.com>
2020-07-03 19:23:46 -0400

    Version 2.5 I added a list for the values.

--- Fibonnaci_series.py
+++ Fibonnaci_series.py
@@ -1,11 +1,15 @@
 print ('This program got the objetive of make the fibonnnaci sequence')
 try:
  entry = int(input())
+ list_1 = []
  def fibonnaci_triangle(number): 
   if number <= 0 or number == 1:
    return number
   elif number > 1:
-   return fibonnaci_triangle(number - 1) + fibonnaci_triangle(number - 2) 
+   result = fibonnaci_triangle(number - 1) + fibonnaci_triangle(number - 2)
+   if result not in list_1:
+    list_1.append(result)
+   return result
  print (fibonnaci_triangle(entry))
 except:
   print ('it not a correct value'' You must to enter a number value')
