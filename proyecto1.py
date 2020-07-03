5a48b1bfb5907b9fe5fc4068d8484c47c1f2aece
Rafael Diaz <nelcidodiaz13@gmail.com>
2020-07-02 20:33:48 -0400

    Final version I hope it is

--- proyecto1.py
+++ proyecto1.py
@@ -1,13 +1,12 @@
-
-x = int(input())
-def fibonnaci_triangle(number): 
- try:
+print ('This program got the objetive of make the fibonnnaci sequence')
+try:
+ x = int(input())
+ def fibonnaci_triangle(number): 
   print (number,end = ' ')
   if number <= 0 or number == 1:
    return number
   elif number > 1:
-   return fibonnaci_triangle(number - 1) + fibonnaci_triangle(number - 2)
- except:
-  return 'Debe ingresar un numero real'
-print (fibonnaci_triangle(x))
-
+   return fibonnaci_triangle(number - 1) + fibonnaci_triangle(number - 2) 
+ print (fibonnaci_triangle(x))
+except:
+  print ('it not a correct value'' You must to enter a number value')
