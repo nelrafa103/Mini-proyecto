bb2f7b21e495813b4a2daefe4f8cb0804be934f7
Rafael Diaz <nelcidodiaz13@gmail.com>
2020-07-04 18:40:45 -0400

    Version 2.5.4

--- Pascal_triangle.py
+++ Pascal_triangle.py
@@ -2,11 +2,17 @@ while True:
  try:
   print ('Enter a real number that you want to get the Pascal_triangle')
   entry = int(input())
-  def pascal_triangle(number):
+  def pascal_triangle(number,i):
+   list_1 = [[]] * number
    if number <= 0:
-    return number
+    return list_1
    else:
-    for x in range(number)
-  print (pascal_triangle(entry))
+    for x in range(i):
+     if len(list_1) > 1 or :
+      list_1.append()
+    else:
+     list_1[0].append(1)
+   return pascal_triangle(number - 1,i + 1)
+  print (pascal_triangle(entry,0))
  except:
   print ('You must to enter a real number')
