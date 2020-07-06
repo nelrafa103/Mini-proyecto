1718daf2a357e78f745a226a3a33a3856c94fd71
Juan Gabriel Tavárez López <58345059+JuanGabriel2960@users.noreply.github.com>
2020-07-06 14:07:53 -0400

    Coach version.
    Improved version of the project.

--- Fibonnaci_series.py
+++ Fibonnaci_series.py
@@ -1,24 +1,39 @@
 import os
-while True:
- try:
-  print ('Enter a real number to get your fibonacci sequence')
-  entry = int(input())
-  if entry < 0:
-   entry *= -1
-  list_1 = []
-  def fibonnaci_triangle(number): 
-   os.system('clear')
-   if number <= 0 or number == 1:
+
+def clear_console():
+  if os.name == "posix":
+    os.system('clear')
+  elif os.name == "ce" or os.name == "nt" or os.name == "dos":
+    os.system('cls')
+
+while(True):
+		try:
+			entry = int(input('How many fibonacci numbers do you want? '))
+			if type(entry) != int:
+				raise ValueError
+			if entry < 1:
+				raise TypeError 
+			break
+		except (ValueError):
+			print('\nEnter a numeric value\n')
+		except (TypeError):
+				print('\nEnter a positive value\n')
+
+list_1 = []
+
+def fibonnaci_triangle(number): 
+  clear_console()
+  if number <= 0 or number == 1:
     return number
-   elif number > 1:
+  elif number > 1:
     result = fibonnaci_triangle(number - 1) + fibonnaci_triangle(number - 2)
-    if result not in list_1:
-     list_1.append(result)
-    if list_1.count(1) < 2:
-     list_1.append(1)
-    if len(list_1) == entry:
-     return list_1
-    return result
-  print (fibonnaci_triangle(entry))
- except:
-  print ('it not a correct value'' You must to enter a number value')
+  if result not in list_1:
+    list_1.append(result)
+  if list_1.count(1) < 2:
+    list_1.append(1)
+  if len(list_1) == entry:
+    return list_1
+  return result
+print (fibonnaci_triangle(entry))
+
+next = input('\nThese are the first {} issues of the fibonacci series...'.format(entry))
--- Pascal_triangle.py
+++ Pascal_triangle.py
@@ -1,24 +1,44 @@
 import os
-while True:
- try:
-  print ('Enter a real number that you want to get the Pascal_triangle')
-  entry = int(input())
-  os.system('clear')
-  def pascal_triangle(number,i,list_2):
-   list_1,r = [],0
-   if number <= 0:
-    return 
-   else:
-    for x in range(i):
-     if len(list_1) == 0 or len(list_1) == i:
-      list_1.append(1)
-     else:
-      list_1.append(sum(list_2[r - 1:r + 1]))
-     r += 1
+
+
+def clear_console():
+    if os.name == "posix":
+        os.system('clear')
+    elif os.name == "ce" or os.name == "nt" or os.name == "dos":
+        os.system('cls')
+
+
+while(True):
+    try:
+        entry = int(input("how many rows of pascal's triangle do you want? "))
+        if type(entry) != int:
+            raise ValueError
+        if entry < 1:
+            raise TypeError
+        break
+    except (ValueError):
+        print('\nEnter a numeric value\n')
+    except (TypeError):
+        print('\nEnter a positive value\n')
+
+
+def pascal_triangle(number, i, list_2):
+    list_1, r = [], 0
+    if number <= 0:
+      return
     else:
-     list_1.append(1)
-   print ( ' ' * number ,list_1)
-   return pascal_triangle(number - 1,i + 1,list_1)
-  print (pascal_triangle(entry + 1,0,[]))
- except:
-  print ('You must to enter a real number')
+        for x in range(i):
+            if len(list_1) == 0 or len(list_1) == i:
+                list_1.append(1)
+            else:
+                list_1.append(sum(list_2[r - 1:r + 1]))
+                r += 1
+        else:
+            list_1.append(1)
+    print(' ' * number, list_1)
+    return pascal_triangle(number - 1, i + 1, list_1)
+
+
+print(pascal_triangle(entry + 1, 0, []))
+
+next = input("\nThe first {} rows of pascal's triangle...".format(entry))
